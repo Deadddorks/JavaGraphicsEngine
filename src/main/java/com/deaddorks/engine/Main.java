37e5df0db724ae88c31be744c830b502e894099c
deadddorks <rudnicki.kalin@uwlax.edu>
2018-03-25 22:03:47 -0500

    decorated

--- src/main/java/com/deaddorks/engine/Main.java
+++ src/main/java/com/deaddorks/engine/Main.java
@@ -23,7 +23,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		
-		UI ui = new UI("Engine 2.0", 500, 500, false)
+		UI ui = new UI("Engine 2.0", 500, 500, false, true)
 		{
 			
 			private Shader shader;
